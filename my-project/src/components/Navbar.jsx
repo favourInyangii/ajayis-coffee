931b082bf7edd4f9f868bbc8eff97653a0ed7fbd
favour ololade <ololadefavourajayi@gmail.com>
2024-08-22 12:55:42 +0100

    navbar creation ended

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MdMenu, MdOutlineShoppingCart } from "react-icons/md";
 import { SiCoffeescript } from "react-icons/si";
+import ResponsiveMenu from './Navbar/ResponsiveMenu';
 
 const NavbarMenu = [
     {
@@ -32,35 +33,42 @@ const NavbarMenu = [
 
 
 const Navbar = () => {
+    const [open, setOpen] = React.useState(false);
     return (
-        <nav>
-            <div className="container flex justify-between items-center py-4">
-                {/*Logo section*/}
-                <div className='text-2xl flex items-center gap-2 font-bold uppercase'>
-                    <p className='text-primary'>Ajayi's</p>
-                    <p className='text-secondary'>Cups</p>
-                    <SiCoffeescript className='text-stone-950' />
+        <>
+            <nav>
+                <div className="container flex justify-between items-center py-4 md:pt-4">
+                    {/*Logo section*/}
+                    <div className='text-2xl flex items-center gap-2 font-bold uppercase'>
+                        <p className='text-primary'>Ajayi's</p>
+                        <p className='text-secondary'>Cups</p>
+                        <SiCoffeescript className='text-stone-950' />
+                    </div>
+                    {/*Menu section*/}
+                    <div className='hidden md:block'>
+                        <ul className='flex items-center gap-6 text-gray-600'>
+                            {NavbarMenu.map((menu) => (
+                                <li key={menu.id}>
+                                    <a href={menu.link}
+                                        className='inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#92400e] font-semibold'> {menu.title} </a>
+                                </li>
+                            ))}
+                            <button className='text-2xl hover:bg-secondary hover:text-white rounded-full p-2 duration-200'>
+                                <MdOutlineShoppingCart />
+                            </button>
+                        </ul>
+                    </div>
+                    {/*mobile hamburger Menu section*/}
+                    <div className='md:hidden' onClick={() =>
+                        setOpen(!open)
+                    }>
+                        <MdMenu className='text-4xl' />
+                    </div>
                 </div>
-                {/*Menu section*/}
-                <div className='hidden md:block'>
-                    <ul className='flex items-center gap-6 text-gray-600'>
-                        {NavbarMenu.map((menu) => (
-                            <li key={menu.id}>
-                                <a href={menu.link}
-                                    className='inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#92400e] font-semibold'> {menu.title} </a>
-                            </li>
-                        ))}
-                        <button className='text-2xl hover:bg-secondary hover:text-white rounded-full p-2 duration-200'>
-                            <MdOutlineShoppingCart />
-                        </button>
-                    </ul>
-                </div>
-                {/*mobile hamburger Menu section*/}
-                <div className='md:hidden'>
-                    <MdMenu className='text-4xl' />
-                </div>
-            </div>
-        </nav>
+            </nav>
+            {/* Mobile Menu section */}
+            <ResponsiveMenu open={open} />
+        </>
     )
 }
 
